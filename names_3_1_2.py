47b395a00c2e14d359e74313140833e6f1107975
Paul Sanyang <paulsanyang@Pauls-MacBook-Pro.local>
2023-12-22 18:33:23 -0500

    First commit

---
+++ names_3_1_2.py
@@ -0,0 +1,14 @@
+''' 3-1 Names'''
+names = ['Louie', 'Ben', 'James', 'Jada', 'Derek']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+# 3.2
+print(f'how are you {names[0]}')
+print(f'how are you {names[1]}')
+print(f'how are you {names[2]}')
+print(f'how are you {names[3]}')
+print(f'how are you {names[4]}')
